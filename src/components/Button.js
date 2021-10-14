9e026bc57a5bd83d923a3b7c60daf89ecbff3532
Maduri Vassaramo <maduri@vinterior.co>
2021-10-14 18:15:33 +0200

    hook up provider to change languageContext

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,16 @@
 import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends React.Component {
+  static contextType = LanguageContext; //To hook up context object to class component
+  //static contextType effectively adds a property to our class itself (rather than instance of the class)
+  // could also be written as Button.contextType = LanguageContext after creating class
+
   render() {
+    const text = this.context === 'english' ? 'Submit' : 'Voorleggen'
+    
     return(
-      <button className="ui button primary">Submit</button>
+      <button className="ui button primary">{text}</button>
     )
   }
 }
---
+++ src/contexts/LanguageContext.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export default React.createContext('english'); //default value is english
